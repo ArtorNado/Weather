465aea6b53b53b6b83b88b7848fc8e737202f130
artornado <16artem1999@mail.ru>
2020-03-06 17:05:13 +0300

    test cicleci

--- app/src/main/java/com/example/weather/ApiFactory.kt
+++ app/src/main/java/com/example/weather/ApiFactory.kt
@@ -7,6 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiFactory {
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private val authInterceptor = Interceptor { chain ->
         // better use separate classes for Interceptors
